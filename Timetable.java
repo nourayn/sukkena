74e4dde3746e4a2ec6f9958b1314b308017ae124
Nourayn <alihannat65@gmail.com>
2024-12-20 14:09:08 -0800

    My 3rd commit

---
+++ Timetable.java
@@ -0,0 +1,29 @@
+/*import java.util.Scanner;
+public class Timetable{
+    public Static void main(String[]args){
+        Scanner y = new Scanner (System.in);
+        System.out.print( please enter number:);
+        double number = y.nextDouble();
+        for(int i = 1; 1 <= 10; i++){
+            System.out.println(number + "x" + i + "=" +(number *i));
+        }
+
+
+    }
+}
+*/
+
+
+import java.util.Scanner;
+
+public class Timetable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
