a8f86b86093137a54682368ecb0f749138380e5a
Nourayn <alihannat65@gmail.com>
2024-12-20 13:56:02 -0800

    My 1st commit

---
+++ Afro.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Afro{
+    public static void main(String[]args){ 
+        Scanner xz = new scanner(System.in);
+        System.out.prinln("enter marks");
+        Double marks = xz.nextDouble();
+        if(marks<=100; || marks >=90 ){
+            
+        }
+    }
+}
